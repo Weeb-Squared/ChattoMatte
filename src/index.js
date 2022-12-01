ee6ba293a5ee8be61b6a8a1cc5dbabac106cb75d
WodaMuszyna <j878025@gmail.com>
2022-12-01 20:02:27 +0100

    Stupid vs code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 import './index.css';
-import reportWebVitals from './__tests__/reportWebVitals';
+import reportWebVitals from './reportWebVitals';
 import Home from "./pages/Home"
 import Login from './pages/Login';
 

95395cf087900e97f5d9afe911912b16aa02db2a
TomaszBachorski <tomekbachorski@o2.pl>
2022-12-01 09:16:21 +0100

    bootstrap and react-router-dom dependencies, Router structure

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom"
+
 import './index.css';
-import App from './App';
 import reportWebVitals from './__tests__/reportWebVitals';
+import Home from "./pages/Home"
+import Login from './pages/Login';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Router>
+    <Routes>
+        <Route exact path="/" element={<Home/>}> </Route>
+        <Route exact path="/login" element={<Login/>}></Route>
+    </Routes>    
+    </Router>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export default function Home () {
+    return (
+        <Link to="/login">Do logowania</Link>
+    );
+}
